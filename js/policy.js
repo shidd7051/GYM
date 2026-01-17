1f49d588a0b96cea8e9691119f1e03a70de78e39
Shiddarth Sharma <shidd7051@gmail.com>
2026-01-17 20:22:39 +0530

    first commit

--- js/policy.js
+++
@@ -1,6 +0,0 @@
-function openMenu(){
-  document.getElementById("mobileMenu").classList.add("active");
-}
-function closeMenu(){
-  document.getElementById("mobileMenu").classList.remove("active");
-}
